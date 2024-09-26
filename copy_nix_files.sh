52d22570814de6a4a44571c677a5abd990049579
Gareth Harding <gdharding@gmail.com>
2024-09-26 15:21:23 +0100

    a2

--- copy_nix_files.sh
+++ copy_nix_files.sh
@@ -14,6 +14,25 @@ if [ -z "$luksuuid" ]; then
 fi
 
 echo $luksuuid
+
+
+
+luksuuid="unset"
+for uuid in /dev/disk/by-uuid/*
+do
+	if test $(readlink -f $uuid) = $rootpart
+	then
+		luksuuid=$uuid
+		break
+	fi
+done
+if [ "$luksuuid" = "unset" ]; then
+  echo "luksuuid is unset. Exiting..."
+  exit 1
+fi
+
+echo $luksuuid
+
 exit
 
 sed -i "s|/dev/disk/by-uuid/<root_partition>|$luksuuid|g" /mnt/etc/nixos/luks.nix
