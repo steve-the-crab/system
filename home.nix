942c141f3dc782ebb78a387a68a2084ef3058e60
Gareth Harding <gdharding@gmail.com>
2024-09-27 00:51:00 +0100

    a2

--- home.nix
+++ home.nix
@@ -1,23 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  # Import the Home Manager module
-  imports = [
+    # Import the Home Manager module
+    imports = [
     <home-manager/nixos>
-  ];
+    ];
 
-  # Enable Home Manager
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-  };
+    users.users.a.isNormalUser = true;
 
+    # Enable Home Manager
+    home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+    };
 
-users.users.a.isNormalUser = true;
-home-manager.users.a = { pkgs, ... }: {
-    home.packages = [ pkgs.zsh ];
-    programs.zsh.enable = true;
-    # The state version is required and should stay at the version you originally installed.
-    home.stateVersion = "24.05";
-};
+    home-manager.users.a = { pkgs, ... }: {
+        home.packages = [ pkgs.zsh ];
+        programs.zsh.enable = true;
+        # The state version is required and should stay at the version you originally installed.
+        home.stateVersion = "24.05";
+    };
 
+}
