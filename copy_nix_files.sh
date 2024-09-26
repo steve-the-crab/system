e274e444a203509dc11fef8d130739292eeb2edf
Gareth Harding <gdharding@gmail.com>
2024-09-26 15:24:51 +0100

    a2

--- copy_nix_files.sh
+++ copy_nix_files.sh
@@ -1,39 +1,19 @@
 #! /bin/sh
 
-rootpart=$1
+device=$1
 
 sudo cp *.nix /etc/nixos/
 
 # Get the UUID of the root partition directly using blkid
-luksuuid1=$(blkid -s UUID -o value $rootpart)
+luksuuid=$(blkid -s UUID -o value $device)
 
 # Check if the UUID was successfully retrieved
-if [ -z "$luksuuid1" ]; then
-    echo "luksuuid1 is unset. Exiting..."
-    # exit 1
-fi
-
-echo $luksuuid1
-
-
-
-luksuuid="unset"
-for uuid in /dev/disk/by-uuid/*
-do
-	if test $(readlink -f $uuid) = $rootpart
-	then
-		luksuuid=$uuid
-		break
-	fi
-done
-if [ "$luksuuid" = "unset" ]; then
-  echo "luksuuid is unset. Exiting..."
-#   exit 1
+if [ -z "$luksuuid" ]; then
+    echo "luksuuid is unset. Exiting..."
+    exit 1
 fi
 
 echo $luksuuid
 
-exit
-
-sed -i "s|/dev/disk/by-uuid/<root_partition>|$luksuuid|g" /mnt/etc/nixos/luks.nix
+sed -i "s|/dev/disk/by-uuid/<root_partition>|/dev/disk/by-uuid/$luksuuid|g" /mnt/etc/nixos/luks.nix
 
