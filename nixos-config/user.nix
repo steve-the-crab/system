29bbaee5083399a9f8df18345fa8a63e8bb4c4e5
Gareth Harding <gdharding@gmail.com>
2024-09-29 18:27:44 +0100

    a2

--- nixos-config/user.nix
+++ nixos-config/user.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
-let
-    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
-in
+# let
+#     home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
+# in
 {
     imports = [
-        (import "${home-manager}/nixos")
+        # (import "${home-manager}/nixos")
     ];
 
     users.users.a = {
@@ -13,16 +13,31 @@ in
         extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     };
 
+    users.defaultUserShell=pkgs.zsh; 
 
-    home-manager.users.a = {
-        /* The home.stateVersion option does not have a default and must be set */
-        home.stateVersion = "24.05";
-        /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
-        home.packages = [ pkgs.zsh ];
 
+    # enable zsh and oh my zsh
+    programs = {
+        zsh = {
+            enable = true;
+            autosuggestions.enable = true;
+            zsh-autoenv.enable = true;
+        }
     };
 
-    programs.zsh.enable = true;
+
+    
+
+
+    # home-manager.users.a = {
+    #     /* The home.stateVersion option does not have a default and must be set */
+    #     home.stateVersion = "24.05";
+    #     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+    #     home.packages = [ pkgs.zsh ];
+
+    # };
+
+    # programs.zsh.enable = true;
 }
 
 
