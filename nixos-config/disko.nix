7cb64e7023c1a8e6cbcb059af7f08e2cad201e64
Gareth Harding <gdharding@gmail.com>
2024-10-04 15:34:21 +0100

    pre update for disko

--- nixos-config/disko.nix
+++ nixos-config/disko.nix
@@ -46,22 +46,22 @@
                       mountpoint = "/home";
                       mountOptions = ["subvol=home" "compress=zstd" "noatime"];
                     };
-                    # "/archive" = {
-                    #   mountpoint = "/archive";
-                    #   mountOptions = ["subvol=home/archive" "compress=zstd" "noatime"];
-                    # };
+                    "/home/archive" = {
+                      mountpoint = "/home/archive";
+                      mountOptions = ["subvol=home/archive" "compress=zstd" "noatime"];
+                    };
                     "/nix" = {
                       mountpoint = "/nix";
                       mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
                     };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                      mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
-                    };
-                    "/log" = {
-                      mountpoint = "/var/log";
-                      mountOptions = ["subvol=log" "compress=zstd" "noatime"];
-                    };
+                    # "/persist" = {
+                    #   mountpoint = "/persist";
+                    #   mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+                    # };
+                    # "/log" = {
+                    #   mountpoint = "/var/log";
+                    #   mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                    # };
                     "/swap" = {
                       mountpoint = "/swap";
                       swap.swapfile.size = "16G";
@@ -91,90 +91,90 @@
 
 
 
-{
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-}
+# {
+#   disko.devices = {
+#     disk = {
+#       main = {
+#         type = "disk";
+#         device = "/dev/vda";
+#         content = {
+#           type = "gpt";
+#           partitions = {
+#             boot = {
+#               size = "1M";
+#               type = "EF02"; # for grub MBR
+#             };
+#             ESP = {
+#               size = "512M";
+#               type = "EF00";
+#               content = {
+#                 type = "filesystem";
+#                 format = "vfat";
+#                 mountpoint = "/boot";
+#                 mountOptions = [ "umask=0077" ];
+#               };
+#             };
+#             root = {
+#               size = "100%";
+#               content = {
+#                 type = "filesystem";
+#                 format = "ext4";
+#                 mountpoint = "/";
+#               };
+#             };
+#           };
+#         };
+#       };
+#     };
+#   };
+# }
 
 
-{
-  disko.devices = {
-    disk {
+# {
+#   disko.devices = {
+#     disk {
 
-    }
-    disk = "/dev/vda";
-    partitions = [
-      {
-        label = "boot";
-        size = 550; # Size in MB
-        type = "ef00"; # EFI partition type
-        format = { fstype = "vfat"; label = "EFI"; options = [ "-F32" ]; };
-        mountPoint = "/boot";
-      }
-      {
-        label = "root";
-        type = "8300"; # Linux root partition type
-        luks = {
-          enable = true;
-          name = "cryptroot";
-          keyFile = null;
-        };
-        lvm = {
-          enable = true;
-          volumeGroup = "vg";
-          logicalVolumes = [
-            {
-              name = "root";
-              size = "100%FREE";
-              format = {
-                fstype = "btrfs";
-                label = "NixOS";
-                options = [ "rw" "noatime" "compress=zstd" "discard=async" "space_cache=v2" "commit=120" ];
-              };
-              subvolumes = [
-                { name = "@"; mountPoint = "/"; }
-                { name = "@home"; mountPoint = "/home"; }
-                { name = "@snapshots"; mountPoint = "/snapshots"; }
-                { name = "@nix"; mountPoint = "/nix"; }
-                { name = "@archive"; mountPoint = "/home/archive"; }
-              ];
-            }
-          ];
-        };
-      }
-    ];
-  };
-}
+#     }
+#     disk = "/dev/vda";
+#     partitions = [
+#       {
+#         label = "boot";
+#         size = 550; # Size in MB
+#         type = "ef00"; # EFI partition type
+#         format = { fstype = "vfat"; label = "EFI"; options = [ "-F32" ]; };
+#         mountPoint = "/boot";
+#       }
+#       {
+#         label = "root";
+#         type = "8300"; # Linux root partition type
+#         luks = {
+#           enable = true;
+#           name = "cryptroot";
+#           keyFile = null;
+#         };
+#         lvm = {
+#           enable = true;
+#           volumeGroup = "vg";
+#           logicalVolumes = [
+#             {
+#               name = "root";
+#               size = "100%FREE";
+#               format = {
+#                 fstype = "btrfs";
+#                 label = "NixOS";
+#                 options = [ "rw" "noatime" "compress=zstd" "discard=async" "space_cache=v2" "commit=120" ];
+#               };
+#               subvolumes = [
+#                 { name = "@"; mountPoint = "/"; }
+#                 { name = "@home"; mountPoint = "/home"; }
+#                 { name = "@snapshots"; mountPoint = "/snapshots"; }
+#                 { name = "@nix"; mountPoint = "/nix"; }
+#                 { name = "@archive"; mountPoint = "/home/archive"; }
+#               ];
+#             }
+#           ];
+#         };
+#       }
+#     ];
+#   };
+# }
